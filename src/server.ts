bd752ed74823964e73bcd05364523520c1f5279a
joschonarth <joschonarth@gmail.com>
2026-03-01 16:25:02 -0300

    chore: setup Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
-import { z } from 'zod'
+import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -14,31 +17,21 @@ app.setValidatorCompiler(validatorCompiler)
 
 app.register(fastifyCors)
 
-app.post(
-  '/subscriptions',
-  {
-    schema: {
-      body: z.object({
-        name: z.string(),
-        email: z.email(),
-      }),
-      response: {
-        201: z.object({
-          name: z.string(),
-          email: z.string(),
-        }),
-      },
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'DevStage',
+      version: '0.0.1',
     },
   },
-  async (request, reply) => {
-    const { name, email } = request.body
+  transform: jsonSchemaTransform,
+})
 
-    return reply.status(201).send({
-      name,
-      email,
-    })
-  }
-)
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(subscribeToEventRoute)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
