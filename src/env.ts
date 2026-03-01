647dd45f10a8fe9ef4a6a8302367d27a684aef2a
joschonarth <joschonarth@gmail.com>
2026-03-01 20:09:32 -0300

    feat: implement access invite link functionality

--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,7 @@ const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   POSTGRES_URL: z.url(),
   REDIS_URL: z.url(),
+  WEB_URL: z.url(),
 })
 
 export const env = envSchema.parse(process.env)
