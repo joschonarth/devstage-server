15996ea1b2045d5b44796098afefce7baa935cc9
joschonarth <joschonarth@gmail.com>
2026-03-01 16:34:39 -0300

    chore: setup env variables

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ import {
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
+import { env } from './env'
 import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -33,6 +34,6 @@ app.register(fastifySwaggerUi, {
 
 app.register(subscribeToEventRoute)
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log('HTTP server running!')
 })
